fade13b8026e5801bd15077db5776d830dd66aaa
KittipongA <kittipong@readyplanet.com>
2018-05-20 14:00:00 +0700

    add code

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id='app' class='container'>
+    <div v-for='(day, index) in weekdays' :key='index' class='weekdays'>
+      <h1>{{ day | capitalize }}</h1>
+      <profile :group='group[index]'></profile>
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+import Profile from './components/Profile.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Profile
+  },
+  data () {
+    return {
+      data: [],
+      weekdays: ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'],
+      group: [[], [], [], [], [], [], []],
+      errors: []
+    }
+  },
+  created () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      axios.get('http://uinames.com/api/?ext&amount=25').then(response => {
+        this.data = response.data
+        this.setGroup()
+      }).catch(e => {
+        this.errors.push(e)
+      })
+    },
+    setGroup () {
+      this.data.forEach(item => {
+        let day = this.getDayOfWeek(item.birthday.mdy)
+        this.group[day].push(item)
+      })
+    },
+    getDayOfWeek (mdy) {
+      let d = new Date(mdy)
+      return d.getDay()
+    }
+  },
+  filters: {
+    capitalize: function (value) {
+      return value.toUpperCase()
+    }
+  }
+}
+</script>
+
+<style>
+.container {
+  display: flex;  
+}
+.weekdays {
+  text-align: center
+}
+</style>
---
+++ src/components/Profile.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class='profile-container'>
+    <div v-for='item in group' :key='item.name' class='avatar'>
+      <img :src='item.photo'>
+      {{ item.name }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Profile',
+  props: ['group']
+}
+</script>
+
+<style scoped>
+.avatar {
+  display: flex;
+  flex-direction: column;
+}
+img {
+  width: 100px;
+}
+</style>
